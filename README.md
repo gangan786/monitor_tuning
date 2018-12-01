d11694339860ce8dfcf05491f659953a6f99c662
gangan786 <215387011@qq.com>
2018-12-01 17:29:09 +0800

    完成环境搭建

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# org.meizhuo
-用于java性能监控与调优
+# Monitor_Tuning
+
+用于java性能监控与调优的学习记录
+
+教程来自慕课网实战教程：[Java生产环境下性能监控与调优详解](https://coding.imooc.com/class/241.html)
+
---
+++ src/main/java/org/meizhuo/monitor_tuning/MonitorTuningApplication.java
@@ -0,0 +1,12 @@
+package org.meizhuo.monitor_tuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorTuningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorTuningApplication.class, args);
+	}
+}
